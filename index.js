1b48b4b591abb38cda5d3ad57e1eed9736f13091
natancejasgarcia <102361572+natancejasgarcia@users.noreply.github.com>
2023-12-09 01:22:45 +0100

    Aqui hago lo que pide el ejercicio 4.3

--- index.js
+++ index.js
@@ -1,101 +1,132 @@
+/**
+ * Tres formas de almacenar valores en memoria en javascript:
+ *      let: se puede modificar
+ *      var: se puede modificar
+ *      const: es constante y no se puede modificar
+ */
+
+// Importamos las bibliotecas necesarias.
+// Concretamente el framework express.
 const express = require("express");
-const mongoose = require("mongoose");
-const helmet = require("helmet");
-const app = express();
-const swaggerUI = require("swagger-ui-express");
+const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+
+// Inicializamos la aplicación
+const app = express();
+
+// Indicamos que la aplicación puede recibir JSON (API Rest)
 app.use(express.json());
-app.use(helmet()); // Añadir seguridad con Helmet
-app.use("/registro", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// Configuración de Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Indicamos el puerto en el que vamos a desplegar la aplicación
 const port = process.env.PORT || 8080;
 
-// Conexión a MongoDB
-mongoose
-  .connect("mongodb://localhost:27017/concesionarioDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Conexión exitosa a MongoDB"))
-  .catch((err) => console.error("No se pudo conectar a MongoDB", err));
-
-// Esquema y modelo de Concesionario
-const cocheSchema = new mongoose.Schema({
-  modelo: String,
-  cv: Number,
-  precio: Number,
-});
-
-const concesionarioSchema = new mongoose.Schema({
-  nombre: String,
-  direccion: String,
-  coches: [cocheSchema],
-});
-
-const Concesionario = mongoose.model("Concesionario", concesionarioSchema);
-
-// GET /concesionarios
-app.get("/concesionarios", async (req, res) => {
-  try {
-    const concesionarios = await Concesionario.find();
-    res.json(concesionarios);
-  } catch (error) {
-    res.status(500).json({ message: "Error al obtener los concesionarios" });
-  }
-});
-
-// POST /concesionarios
-app.post("/concesionarios", async (req, res) => {
-  const nuevoConcesionario = new Concesionario(req.body);
-  try {
-    await nuevoConcesionario.save();
-    res.status(201).json(nuevoConcesionario);
-  } catch (error) {
-    res.status(400).json({ message: "Error al crear el concesionario" });
-  }
-});
-
-// GET /concesionarios/:id
-app.get("/concesionarios/:id", async (req, res) => {
-  try {
-    const concesionario = await Concesionario.findById(req.params.id);
-    if (!concesionario) {
-      return res.status(404).send("Concesionario no encontrado");
-    }
-    res.json(concesionario);
-  } catch (error) {
-    res.status(500).json({ message: "Error al obtener el concesionario" });
-  }
-});
-
-// PUT /concesionarios/:id
-app.put("/concesionarios/:id", async (req, res) => {
-  try {
-    const concesionarioActualizado = await Concesionario.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!concesionarioActualizado) {
-      return res.status(404).send("Concesionario no encontrado");
-    }
-    res.json(concesionarioActualizado);
-  } catch (error) {
-    res.status(400).json({ message: "Error al actualizar el concesionario" });
-  }
-});
-
-// DELETE /concesionarios/:id
-app.delete("/concesionarios/:id", async (req, res) => {
-  try {
-    const concesionarioEliminado = await Concesionario.findByIdAndDelete(req.params.id);
-    if (!concesionarioEliminado) {
-      return res.status(404).send("Concesionario no encontrado");
-    }
-    res.json({ message: "Concesionario eliminado" });
-  } catch (error) {
-    res.status(500).json({ message: "Error al eliminar el concesionario" });
-  }
-});
-
-// Las rutas para los coches se manejarán de manera similar, utilizando el modelo `Concesionario`
-// y aplicando las operaciones correspondientes en el array `coches`.
-
+// Arrancamos la aplicación
 app.listen(port, () => {
-  console.log(`Servidor desplegado en puerto: ${port}`);
+  console.log(`Server running on port ${port}`);
+});
+
+// Definimos una estructura de datos
+// (temporal hasta incorporar una base de datos)
+
+let concesionarios = [
+  {
+    id: 1,
+    nombre: "Concesionario 1",
+    direccion: "Dirección 1",
+    coches: [
+      { marca: "Renault", modelo: "Clio" },
+      { marca: "Nissan", modelo: "Skyline R34" },
+    ],
+  },
+  {
+    id: 2,
+    nombre: "Concesionario 2",
+    direccion: "Dirección 2",
+    coches: [
+      { marca: "Ferrari", modelo: "Clio" },
+      { marca: "Audi", modelo: "Skyline R34" },
+    ],
+  },
+  {
+    id: 3,
+    nombre: "Concesionario 3",
+    direccion: "Dirección 3",
+    coches: [
+      { marca: "Renault", modelo: "Clio" },
+      { marca: "Nissan", modelo: "Skyline R34" },
+    ],
+  },
+  {
+    id: 4,
+    nombre: "Concesionario 4",
+    direccion: "Dirección 4",
+    coches: [
+      { marca: "Ferrari", modelo: "Clio" },
+      { marca: "Audi", modelo: "Skyline R34" },
+    ],
+  },
+];
+// Lista todos los coches
+app.get("/concesionarios", (request, response) => {
+  console.log("Accediendo a /concesionarios");
+  console.log(concesionarios);
+  response.json(concesionarios);
+});
+
+// Añadir un nuevo concesionario
+app.post("/concesionarios", (request, response) => {
+  console.log("Accediendo a /concesionarios");
+  console.log(concesionarios);
+  const nuevoConcesionario = request.body;
+
+  // Encuentra el ID más alto actual en la lista de concesionarios
+  const maxId = concesionarios.reduce((max, concesionario) => (concesionario.id > max ? concesionario.id : max), 0);
+
+  // Asigna un nuevo ID que es uno más grande que el máximo actual
+  nuevoConcesionario.id = maxId + 1;
+
+  // Agrega el nuevo concesionario a la lista
+  concesionarios.push(nuevoConcesionario);
+
+  response.json({ message: "Concesionario creado", id: nuevoConcesionario.id });
+});
+
+// Obtener un solo coche
+app.get("/concesionarios/:id", (request, response) => {
+  console.log("Accediendo a /concesionarios");
+  console.log(concesionarios);
+  const id = parseInt(request.params.id);
+  const concesionario = concesionarios.find((c) => c.id === id);
+  if (concesionario) {
+    response.json(concesionario);
+  } else {
+    response.status(404).send("Concesionario no encontrado");
+  }
+});
+
+// Actualizar un solo consionario
+app.put("/concesionarios/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const index = concesionarios.findIndex((c) => c.id === id);
+  if (index !== -1) {
+    concesionarios[index] = { ...concesionarios[index], ...request.body };
+    response.json({ message: "Concesionario actualizado" });
+  } else {
+    response.status(404).send("Concesionario no encontrado");
+  }
+});
+
+// Borrar un elemento del array
+app.delete("/concesionarios/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const index = concesionarios.findIndex((c) => c.id === id);
+  if (index !== -1) {
+    concesionarios.splice(index, 1);
+    response.json({ message: "Concesionario eliminado" });
+  } else {
+    response.status(404).send("Concesionario no encontrado");
+  }
 });
